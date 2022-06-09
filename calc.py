7592cc6e43fbf86e29ebaa6a4c9706382869f416
Wish <wishma.samaraweera@monash.edu>
2022-06-09 10:17:14 +0800

    Changing for checking

--- calc.py
+++ calc.py
@@ -10,13 +10,3 @@ def compute(expression):
     else:
         print('unknown operator!')
         return None
-        
-def compute(expression):
-    num0, operator, num1 = expression.split(' ')
-    if operator == '+':
-        return num0 + num1
-    elif operator == '-':
-        return num0 - num1
-    else:
-        print('unknown operator!')
-        return None
